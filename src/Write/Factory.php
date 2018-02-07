e48c7d8137bebeb9c664fd37bd424991f6e820c1
dzer <358654744@qq.com>
2018-02-07 15:15:09 +0800

    fix(write): 修改write类路径问题

--- src/Write/Factory.php
+++ src/Write/Factory.php
@@ -5,7 +5,7 @@ namespace Mll\Write;
 use Mll\Core\Container;
 
 /**
- * 缓存工厂类
+ * 写入层工厂类
  *
  * @package Mll\Cache
  * @author wang zhou <zhouwang@mll.com>
@@ -16,7 +16,7 @@ class Factory
     public static function getInstance($driver = 'write', $config = [])
     {
         $driver = ucfirst(strtolower($driver));
-        $className = __NAMESPACE__ . $driver;
+        $className = __NAMESPACE__ . '\\' . $driver;
         return Container::getInstance($className, $config);
     }
 }
