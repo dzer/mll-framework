3d6fd0bc2d132515504d00651f21893251eb7c7a
dzer <358654744@qq.com>
2018-03-01 16:46:35 +0800

    feat(model): 增加对象池调用

--- src/Service/Service.php
+++ src/Service/Service.php
@@ -1,17 +1,17 @@
 <?php
 
-namespace Mll\Model;
+namespace Mll\Service;
 
 use Mll\Core\Container;
 
 /**
- * model
+ * service
  *
  * @package Mll
  * @author Xu Dong <d20053140@gmail.com>
  * @since 1.0
  */
-class Model
+class Service
 {
     /**
      * 对象池
